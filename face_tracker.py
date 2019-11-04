d353782d0542bbf49d1a3343cfcf9629e2a8b717
Alexey Egorov <egorovalexeyd@gmail.com>
2019-11-04 13:34:11 +0300

    tracking optimization and time counting add

--- face_tracker.py
+++ face_tracker.py
@@ -2,8 +2,32 @@ import face_detector, cv2
 
 class TemplateTracking:
 
-    def do_Track_in_Frame(self, template, image):
-        res = cv2.matchTemplate(image, template, cv2.TM_SQDIFF_NORMED)
+
+    # template -- template for tracking; position -- template position on image; image - cur image
+    def do_Track_in_Frame(self, template, position, image):
+        (x, y, w, h) = self.area_To_Track(position, (len(image[0]), len(image)))
+        res = cv2.matchTemplate(image[y:y+h, x:x+w], template, cv2.TM_SQDIFF_NORMED)
         miv_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        min_loc = (min_loc[0] + x, min_loc[1] + y)
+        max_loc = (max_loc[0] + x, max_loc[1] + y)
         return miv_val, max_val, min_loc, max_loc
+
+    def area_To_Track(self, position, max_size, coef = 2):
+        x1 = position[0] - position[2]//2
+        y1 = position[1] - position[3]//2
+        x2 = position[0] + position[2]*3//2
+        y2 = position[1] + position[3]*3//2
+
+        if x1 < 0:
+            x1 = 0
+        if y1 < 0:
+            y1 = 0
+        if x2 > max_size[0]:
+            x2 = max_size[0]
+        if y2 > max_size[1]:
+            y2 = max_size[1]
+
+        return (x1, y1, x2 - x1, y2 - y1)
+
+
 
--- processing.py
+++ processing.py
@@ -5,6 +5,8 @@ from face_detector import HaarFaceDetector
 from face_tracker import TemplateTracking
 import util
 
+debug_Time = True
+debug_Tracking_Results = False
 webcam = camera.default_webcamera(1280, 960)
 detector = HaarFaceDetector("haarcascade_frontalface_default.xml")
 tracker = TemplateTracking()
@@ -21,18 +23,26 @@ while True:
     grayFrame = frame[:, :]
     cv2.cvtColor(grayFrame, cv2.COLOR_BGR2GRAY)
 
+    if debug_Time:
+        curTime = time.time()
+        faceToTrack = len(faces)
 
     if faces:
         index = 0
         while index < len(faces):
             face = faces[index]
-            min, max, minLoc, maxLoc = tracker.do_Track_in_Frame(face.mat, grayFrame)
-            print(f'ID {face.id} Min {min}  Max {max}')
+            min, max, minLoc, maxLoc = tracker.do_Track_in_Frame(face.mat, face.pos, grayFrame)
+            if debug_Tracking_Results:
+                print(f'ID {face.id} Min {min}  Max {max}')
             if min > 0.05:
                 del faces[index]
             else:
                 cv2.rectangle(frame, minLoc, (minLoc[0] + face.cows(), minLoc[1] + face.rows()), (0, 255, 255), 2)
                 index += 1
+
+    if debug_Time:
+        print(f'We spent {round((time.time()-curTime)*1000,2)} ms to track {faceToTrack} objects on one frame')
+        curTime = time.time()
 
     faces_from_det = detector.detect(grayFrame)
 
@@ -45,9 +55,11 @@ while True:
                     is_new = False
 
         if is_new:
-            faces.append(Face(frame[y:y+h, x:x+w], (x, y, w, h), id))
+            faces.append(Face(grayFrame[y:y+h, x:x+w], (x, y, w, h), id))
             id += 1
 
+    if debug_Time:
+        print(f'We sepnt {round((time.time()-curTime)*1000,2)} ms to detect faces on one frame')
 
 
     frame = cv2.flip(frame, 1)
